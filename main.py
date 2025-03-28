a8b2183db6b6da4a66579f6d9ec54fe7d7632e52
Antonio Alberto Giannalia <antonio.giannalia@truelink.it>
2025-03-28 17:51:19 +0100

    Update main

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pygame
+import datetime
 
 from constants import *
 
 def main():
     pygame.init()
+    pygame.time.Clock()
+    delta_time = 0
+
+    pygame.display.set_caption("Asteroids")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.flip()
 
@@ -13,6 +18,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        time_passed = pygame.time.Clock().tick(60)
+        delta_time += time_passed / 1000.0
     print("Starting Asteroids!")
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
